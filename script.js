9bd84595a5d0d9845d1fcc2836c77345a632aeb9
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2023-06-15 19:35:04 +0700

    add file javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function music(){
+    var audio = document.getElementById("audio").value;
+    audio.play();
+}
+
+function nama1(){
+    let nama =document.getElementById("myText").value;
+    // document.getElementById("demo").innerHTML = nama;
+    alert('Salam kenal ka ' + nama);
+}
